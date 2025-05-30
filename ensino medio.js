1ef3d2bac226dd125967782275d0d2021d094055
Universidade POC <universidadepoc@gmail.com>
2025-05-30 11:09:19 -0300

    Delete ensino medio.js

--- ensino medio.js
+++
@@ -1,8 +0,0 @@
-// Botão de voltar
-const botaoVoltar = document.getElementById("voltar-btn");
-if (botaoVoltar) {
-  botaoVoltar.addEventListener("click", e => {
-    e.preventDefault();
-    window.location.href = "index.html"; // Altere conforme necessário
-  });
-}
